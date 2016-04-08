504efb54804a02f82f37e35d116db7053c2f19a6
asapplications <as.appdevelopment@gmail.com>
2016-04-08 17:35:17 +0200

    Update api.js

--- api.js
+++ api.js
@@ -13,14 +13,19 @@
     };
 
     ext.get_question_title = function(question_number,quiz_id) {
-        return httpGet("https://quizmaker.pythonanywhere.com/api/get_title/"+question_number+"/"+quiz_id);
+        return httpGet("https://quizmaker.pythonanywhere.com/api/get_title/"+quiz_id+"/"+question_number);
     };
+    ext.get_answers = function(question_number,quiz_id,answer_var) {
+        console.log
+        //return httpGet("https://quizmaker.pythonanywhere.com/api_get_answers/"+quiz_id+"/"+question_number);
+    }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
             ['r', 'Get Question Title No. %n of Quiz with id %n', 'get_question_title', 1, 0],
+            ["r", "Get All Answers from Question %n of Quiz %n in the list variable %n", 1,0 , ["hi"]]
         ]
     };
 
